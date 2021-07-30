603d34e70390a34a04ee9c47887f8f239a8d40c5
guozhenyu02 <guozhenyu02@meituan.com>
2021-07-30 17:07:36 +0800

    这是我本地第二次修改提交。

--- practice_git/src/Test1.java
+++ practice_git/src/Test1.java
@@ -2,5 +2,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("这是我的远程修改。");
         System.out.println("Hello World");
+        System.out.println("这是我的本地修改。");
     }
 }
