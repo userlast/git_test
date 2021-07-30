aa790d8e02614d2b177fb92f3393615f784eb48f
guozhenyu02 <guozhenyu02@meituan.com>
2021-07-30 17:43:02 +0800

    本地分支->远程分支

--- practice_git/src/Test1.java
+++ practice_git/src/Test1.java
@@ -4,6 +4,7 @@ public class Test1 {
         System.out.println("Hello World");
         System.out.println("这是我的本地修改。");
         System.out.println("这是我的远程分支修改1。");
-        System.out.println("这是我的本地分支mylocalbranch修改。");
+        System.out.println("这是我的本地分支mylocalbranch修改1。");
+        System.out.println("这是我的本地分支mylocalbranch修改2。");
     }
 }
